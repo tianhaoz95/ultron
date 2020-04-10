0696049c3dd4363ede00636fcdab94fd4c92cc56
Tianhao Zhou <tianhaoz@umich.edu>
2020-04-09 21:47:54 -0700

    dev: refactor cartpole (#2)

---
+++ tools/hw_checker.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import GPUtil
+
+def check():
+  print('Tensorflow version' + tf.__version__)
+  try:
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver()  # TPU detection
+    print('Running on TPU ', tpu.cluster_spec().as_dict()['worker'])
+    tf.config.experimental_connect_to_cluster(tpu)
+    tf.tpu.experimental.initialize_tpu_system(tpu)
+    tf.distribute.experimental.TPUStrategy(tpu)
+  except ValueError:
+    print('ERROR: Not connected to a TPU runtime!')
+    GPUs = GPUtil.getGPUs()
+    print('GPU count: ' + str(len(GPUs)))
