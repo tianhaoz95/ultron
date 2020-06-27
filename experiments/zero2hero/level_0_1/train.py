c8f0101d7a5f8f8bcaff2e3bd5b0d437e1fbee84
Tianhao Zhou <tianhaoz@umich.edu>
2020-06-26 23:14:17 -0700

    dev

--- experiments/zero2hero/level_0_1/train.py
+++ experiments/zero2hero/level_0_1/train.py
@@ -5,32 +5,38 @@ from tensorflow.keras.layers import Dense
 
 
 def collect_step(env, policy, buffer):
+    action = policy.action()
     next_state, reward, done, _ = env.step(action)
     buffer.record(reward, next_state)
 
 
 class DqnAgent(object):
     def __init__(self):
-        pass
+        self.next_q_net = self.build_dqn_model()
+        self.current_q_net = self.build_dqn_model()
+
+    def build_dqn_model(self, observation_space, action_space):
+        q_net = Sequential()
+        q_net.add(Dense(100, input_shape=(None, observation_space), activation='relu'))
+        q_net.add(Dense(action_space))
+        return q_net
 
 
-class DqnBuffer(object):
+class DqnReplayBuffer(object):
     def __init__(self):
         pass
 
 
 def train_model(
-    num_iterations=config.default_num_iterations,
-    collect_steps_per_iteration=config.default_collect_steps_per_iteration,
+        num_iterations=config.default_num_iterations,
+        collect_steps_per_iteration=config.default_collect_steps_per_iteration,
 ):
     env_name = config.default_env_name
-    q_net = Sequential()
     train_env = gym.make(env_name)
     print('Action space: ')
     print(train_env.action_space)
     print('Observation space: ')
     print(train_env.observation_space)
-    q_net.add(Dense(100, input_shape=(None, 6), activation='relu'))
-    q_net.add(Dense(1))
     for eps_cnt in range(num_iterations):
         for step_cnt in range(collect_steps_per_iteration):
+            collect_step(train_env, agent.collect_policy, buffer)
